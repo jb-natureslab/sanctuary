3d4943fcfc6c4c1c59afe626fdaaf5e423ffc39f
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-04 18:58:22 +0000

    form

--- site/snippets/blocks/signup.php
+++ site/snippets/blocks/signup.php
@@ -10,6 +10,13 @@
 	      <h2 id="signup">Register Your Interest</h2>
 		  <label for="first-name">First name</label>
 		  <input id="first-name" type="text" name="metadata__first-name" required placeholder="John" />
+		  <label for="organisation">Type of Organisation</label>
+		  <select id="organisation" name="metadata__organisation" required>
+			  <option value="individual">Individual</option>
+			  <option value="church">Church</option>
+			  <option value="community-group">Community Group</option>
+			  <option value="business">Business</option>
+		  </select>
 		  <label for="bd-email">Enter your email</label>
 		  <input type="email" name="email" id="bd-email" required placeholder="john@example.com" />
 		  <input type="submit" value="Keep Me Informed" class="button" />
