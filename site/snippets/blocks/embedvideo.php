10b967e5d19d5aa055aecc98978eea3e0400107f
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-04 21:03:39 +0000

    video

--- site/snippets/blocks/embedvideo.php
+++ site/snippets/blocks/embedvideo.php
@@ -1,6 +1,6 @@
 <div class="wrapper wrapper--video">
 	<div class="wrapper--video_restrict flow">
 		<img src="/assets/images/video.svg" alt="Watch the Video" />
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/jGzZFmR6_Pc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/KZXPzqskZuk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
 </div>
