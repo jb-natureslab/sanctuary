754703639c74ab1369a8a0aee638d8b40dc29376
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-06 10:23:35 +0000

    endorsements

---
+++ site/snippets/blocks/endoresments.php
@@ -0,0 +1,11 @@
+<div class="wrapper wrapper--endorsements">
+	<div class="wrapper--endorsements_restrict flow">
+		<h2>Endorsements</h2>
+		<ul class="cards">
+			<li>
+				<img src="/assets/images/ruth.jpeg" alt="Rt Rev'd Ruth Bushyager, Bishop of Horsham" />
+				<blockquote>&ldquo;It is easy to feel helpless and hopeless watching the plight of Ukrainians as their lives are devastated and as so many flee in search of refuge. But we CAN be proactive - we can get ready to provide welcome, support and community for those who make it to the UK. I signed up to the Sanctuary Foundation as soon as registration opened. It means I am on the list to meet refugees at the airport, and pledge to befriend and help a family. It also means the Government knows there are huge numbers of us wanting to be a blessing to refugees - we are poised and ready to help. Can I encourage you to sign up too?&rdquo;</blockquote>
+				<small>&#8212; Rt Rev'd Ruth Bushyager, Bishop of Horsham</small>
+		</ul>
+	</div>
+</div>
---
+++ site/snippets/blocks/twitterfeed.php
@@ -0,0 +1,6 @@
+<div class="wrapper wrapper--twitter">
+	<div class="wrapper--twitter_restrict flow">
+		<h2>Join the Conversation on Twitter</h2>
+		<a href="https://twitter.com/intent/tweet?button_hashtag=UkraineSanctuary&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #UkraineSanctuary</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+	</div>
+</div>
