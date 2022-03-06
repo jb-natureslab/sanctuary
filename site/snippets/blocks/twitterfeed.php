0d889eeb6e7ef43341feb9a9b462f4e0d69ebbe3
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-06 10:42:15 +0000

    juicer

--- site/snippets/blocks/twitterfeed.php
+++ site/snippets/blocks/twitterfeed.php
@@ -1,6 +1,8 @@
 <div class="wrapper wrapper--twitter">
 	<div class="wrapper--twitter_restrict flow">
 		<h2>Join the Conversation on Twitter</h2>
-		<a href="https://twitter.com/intent/tweet?button_hashtag=UkraineSanctuary&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #UkraineSanctuary</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
+		<link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
+		<ul class="juicer-feed" data-feed-id="ukrainesanctuary"></ul>
 	</div>
 </div>
