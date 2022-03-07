b14fb9e0e987a36cf971a24623529f8501a1c5b6
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-07 17:56:33 +0000

    cat

--- site/snippets/blocks/endoresments.php
+++ site/snippets/blocks/endoresments.php
@@ -3,6 +3,9 @@
 		<h2>Endorsements</h2>
 		<ul class="cards">
 			<li>
+				<img src="/assets/images/cat.jpeg" alt="Cat Ross, CEO, Baby Basics" />
+				<blockquote>&ldquo;At Baby Basics we call on all our supporters to consider registering their interest in becoming a humanitarian sponsor to welcome Ukrainian refugees to the UK. Baby Basics is committed, prepared and ready to support Ukrainian families with children from 0-5yrs and their humanitarian sponsors. We want to ensure they have all the essentials they need for life in the UK, including ensuring each child has a safe space to sleep, toys, clothing, prams, feeding equipment, toiletries & nappies.&rdquo;</blockquote>
+				<small>&#8212; Cat Ross, CEO, Baby Basics</small>
 				<img src="/assets/images/ruth.jpeg" alt="Rt Rev'd Ruth Bushyager, Bishop of Horsham" />
 				<blockquote>&ldquo;It is easy to feel helpless and hopeless watching the plight of Ukrainians as their lives are devastated and as so many flee in search of refuge. But we CAN be proactive - we can get ready to provide welcome, support and community for those who make it to the UK. I signed up to the Sanctuary Foundation as soon as registration opened. It means I am on the list to meet refugees at the airport, and pledge to befriend and help a family. It also means the Government knows there are huge numbers of us wanting to be a blessing to refugees - we are poised and ready to help. Can I encourage you to sign up too?&rdquo;</blockquote>
 				<small>&#8212; Rt Rev'd Ruth Bushyager, Bishop of Horsham</small>
