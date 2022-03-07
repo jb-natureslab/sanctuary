6572f976be101a1dc5290eb6fdf9f7459df7305c
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-07 17:56:42 +0000

    Update endoresments.php

--- site/snippets/blocks/endoresments.php
+++ site/snippets/blocks/endoresments.php
@@ -3,7 +3,7 @@
 		<h2>Endorsements</h2>
 		<ul class="cards">
 			<li>
-				<img src="/assets/images/cat.jpeg" alt="Cat Ross, CEO, Baby Basics" />
+				<img src="/assets/images/cat.jpg" alt="Cat Ross, CEO, Baby Basics" />
 				<blockquote>&ldquo;At Baby Basics we call on all our supporters to consider registering their interest in becoming a humanitarian sponsor to welcome Ukrainian refugees to the UK. Baby Basics is committed, prepared and ready to support Ukrainian families with children from 0-5yrs and their humanitarian sponsors. We want to ensure they have all the essentials they need for life in the UK, including ensuring each child has a safe space to sleep, toys, clothing, prams, feeding equipment, toiletries & nappies.&rdquo;</blockquote>
 				<small>&#8212; Cat Ross, CEO, Baby Basics</small>
 				<img src="/assets/images/ruth.jpeg" alt="Rt Rev'd Ruth Bushyager, Bishop of Horsham" />
