76a676badd7c68be7204eb401f6dd6a563352021
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-08 16:55:33 +0000

    Update hero.php

--- site/snippets/blocks/hero.php
+++ site/snippets/blocks/hero.php
@@ -1,6 +1,6 @@
 <div class="wrapper wrapper--hero">
 	<?php if($image = $block->photo()->toFile()): ?>
-	
+	<img src="<?= $image->resize(1024)->url() ?>" alt="<?= $block->photoalt() ?>">
 	
 	<?php endif ?>
 	<div class="wrapper--hero_text">
