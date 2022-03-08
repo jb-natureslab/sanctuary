02a1c4cdad109b937923a020e7e81beec313c36c
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-08 16:24:50 +0000

    optimise fonts

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -22,9 +22,13 @@
 	<link rel="preconnect" href="https://www.youtube.com">
 	<link rel="preconnect" href="https://assets.juicer.io">
 	
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">  
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Roboto:wght@700&display=swap" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Roboto:wght@700&display=swap" media="print" onload="this.media='all'" />
+	<noscript>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Roboto:wght@700&display=swap" />
+	</noscript>
+	
 	<?= css('/assets/css/stylesheet.css?v='.rand()) ?>
 </head>
 <body>
