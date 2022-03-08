8dca40c685ece95a86c91eb4cbf70d91f773206f
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-08 16:44:45 +0000

    Update twitterfeed.php

--- site/snippets/blocks/twitterfeed.php
+++ site/snippets/blocks/twitterfeed.php
@@ -1,8 +1,16 @@
 <div class="wrapper wrapper--twitter">
 	<div class="wrapper--twitter_restrict flow">
 		<h2>Join the Conversation on Twitter</h2>
-		<script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
-		<link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
-		<ul class="juicer-feed" data-feed-id="ukrainesanctuary"></ul>
+		<script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+		<script src="//assets.juicer.io/embed-amd.js" type="text/javascript"></script>
+		<link href='//assets.juicer.io/embed.css' media='all' rel='stylesheet' type='text/css' />
+		<ul class="juicer-feed" data-feed-id="ukrainesanctuary"></ul>   
+		<script>
+			 $(window).scroll(function() {
+		        if($(window).scrollTop()>'500'){
+		            Juicer.initialize();
+		        }
+		    });
+		</script>
 	</div>
 </div>
