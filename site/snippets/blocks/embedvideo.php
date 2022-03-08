434702c9eda9bd802d0e80d55dfdd61d43f71897
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-08 16:07:23 +0000

    optimised video

--- site/snippets/blocks/embedvideo.php
+++ site/snippets/blocks/embedvideo.php
@@ -1,6 +1,15 @@
 <div class="wrapper wrapper--video">
 	<div class="wrapper--video_restrict flow">
 		<img src="/assets/images/video.svg" alt="Watch the Video" />
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/m4PaAOfRzEg?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe
+		  width="560"
+		  height="315"
+		  src="https://www.youtube.com/embed/Y8Wp3dafaMQ&autoplay=1"
+		  srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/m4PaAOfRzEg?autoplay=1><img src=https://img.youtube.com/vi/m4PaAOfRzEg/hqdefault.jpg alt='Sanctuary Foundation'><span>▶</span></a>"
+		  frameborder="0"
+		  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+		  allowfullscreen
+		  title="Sanctuary Foundation"
+		></iframe>
 	</div>
 </div>
