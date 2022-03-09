069f66441ff62db78d4c1353bcbcff51cf9cf18b
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-09 10:52:52 +0000

    Update getUsers.php

--- getUsers.php
+++ getUsers.php
@@ -18,7 +18,7 @@ if (curl_errno($ch)) {
 curl_close($ch);
 $result = json_decode($result,true);
 print_r($result);
-foreach($result[0] as $user){
+foreach($result['results'] as $user){
 	print_r($user);
 	echo "<hr />";
 }
