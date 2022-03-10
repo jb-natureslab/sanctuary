c453dc9610c3875eec44a98bd27db5d75d2c4cb8
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-10 14:37:51 +0000

    mailchimp

--- site/snippets/blocks/signup.php
+++ site/snippets/blocks/signup.php
@@ -1,36 +1,54 @@
 <div class="wrapper wrapper--form" id="pledge">
 	<div class="wrapper--form_restrict flow">
-		<form
-		  action="https://buttondown.email/api/emails/embed-subscribe/sanctuaryfoundation"
-		  method="post"
-		  target="popupwindow"
-		  onsubmit="window.open('https://buttondown.email/sanctuaryfoundation', 'popupwindow')"
-		  class="embeddable-buttondown-form"
-		>
-	      <h2 id="signup">Pledge Your Support</h2>
-		  <label for="name">Name</label>
-		  <input id="name" type="text" name="metadata__name" required placeholder="John" />
-		  
-		  <label for="bd-email">Email</label>
-		  <input type="email" name="email" id="bd-email" required placeholder="john@example.com" />
-		  
-		  <label for="phone">Phone</label>
-		  <input id="phone" type="text" name="metadata__phone" />
-		  
-		  <label for="organisation">Type of Organisation</label>
-		  <select id="organisation" name="tag" required>
-			  <option value="Individual">Individual</option>
-			  <option value="Church">Church</option>
-			  <option value="School">School</option>
-			  <option value="Community Group">Community Group</option>
-			  <option value="Business">Business</option>
-		  </select>
-		  
-		  <p>Please read our <a href="/privacy">privacy statement</a> before submitting this form. By pledging your support you are not committing to any specific form of help at this point, as the details of the scheme are yet to be published.</p>
-		  
-		  <p>After submitting this form you will be sent an email asking you to confirm your email address. Please click the link in the email to confirm your address - this will complete the process.</p>
-		  
-		  <input type="submit" value="I Pledge My Support" class="button" />
-		</form>
+		<!-- Begin Mailchimp Signup Form -->
+		<div id="mc_embed_signup">
+			<form action="https://sanctuaryfoundation.us14.list-manage.com/subscribe/post?u=d83e529d923280bf88b70fede&amp;id=b39a4c8f20" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    <div id="mc_embed_signup_scroll">
+				<h2>Pledge Your Support</h2>
+				<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+				<div class="mc-field-group">
+					<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+				</label>
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+				</div>
+				<div class="mc-field-group">
+					<label for="mce-FNAME">First Name </label>
+					<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+				</div>
+				<div class="mc-field-group size1of2">
+					<label for="mce-PHONE">Phone Number </label>
+					<input type="text" name="PHONE" class="" value="" id="mce-PHONE">
+				</div>
+				<div class="mc-field-group input-group">
+				    <label>Organisation </label>
+				    <ul><li><input type="radio" value="1" name="group[45229]" id="mce-group[45229]-45229-0" checked><label for="mce-group[45229]-45229-0">Individual</label></li>
+				<li><input type="radio" value="2" name="group[45229]" id="mce-group[45229]-45229-1"><label for="mce-group[45229]-45229-1">Church</label></li>
+				<li><input type="radio" value="4" name="group[45229]" id="mce-group[45229]-45229-2"><label for="mce-group[45229]-45229-2">Commuity Group</label></li>
+				<li><input type="radio" value="8" name="group[45229]" id="mce-group[45229]-45229-3"><label for="mce-group[45229]-45229-3">Business</label></li>
+				<li><input type="radio" value="16" name="group[45229]" id="mce-group[45229]-45229-4"><label for="mce-group[45229]-45229-4">School</label></li>
+				<li><input type="radio" value="32" name="group[45229]" id="mce-group[45229]-45229-5"><label for="mce-group[45229]-45229-5">Neighbourhood</label></li>
+				</ul>
+				
+				<p>Please read our <a href="/privacy">privacy statement</a> before submitting this form. By pledging your support you are not committing to any specific form of help at this point, as the details of the scheme are yet to be published.</p>
+				  
+				<p>After submitting this form you will be sent an email asking you to confirm your email address. Please click the link in the email to confirm your address - this will complete the process.</p>
+				  
+				</div>
+					<div id="mce-responses" class="clear foot">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d83e529d923280bf88b70fede_b39a4c8f20" tabindex="-1" value=""></div>
+				</div>
+				
+				<div class="optionalParent">
+		            <div class="clear foot">
+		                <input type="submit" value="I Pledge My Support" name="subscribe" id="mc-embedded-subscribe" class="button">
+		            </div>
+		        </div>
+			</form>
+		</div>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		<!--End mc_embed_signup-->
 	</div>
 </div>
