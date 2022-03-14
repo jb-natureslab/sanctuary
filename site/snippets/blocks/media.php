0f523d4fee96fb37885b00e6c08a651d578c871d
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-14 21:08:26 +0000

    twtitter

--- site/snippets/blocks/media.php
+++ site/snippets/blocks/media.php
@@ -1,7 +1,10 @@
 <div class="wrapper wrapper--twitter" id="media">
 	<div class="wrapper--twitter_restrict flow">
-		<h2>Press Coverage</h2>
-		<blockquote class="twitter-tweet" data-partner="tweetdeck"><p lang="en" dir="ltr">More than two thousand companies, churches and individuals have offered to help host Ukrainian refugees fleeing the war. <br><br>Krish Kandiah from the Sanctuary Foundation tells <a href="https://twitter.com/hashtag/BBCBreakfast?src=hash&amp;ref_src=twsrc%5Etfw">#BBCBreakfast</a> the response has been unprecedented .<a href="https://t.co/JdS0rfuOox">https://t.co/JdS0rfuOox</a> <a href="https://t.co/trIAQjE5rF">pic.twitter.com/trIAQjE5rF</a></p>&mdash; BBC Breakfast (@BBCBreakfast) <a href="https://twitter.com/BBCBreakfast/status/1502174733630156800?ref_src=twsrc%5Etfw">March 11, 2022</a></blockquote>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<h2>On Twitter</h2>
+		<div class="tweets">
+			<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Krish Kandiah is founder of the Sanctuary Project which is helping to co-ordinate hosts with Ukrainian refugees. <br><br>He tells <a href="https://twitter.com/hashtag/BBCBreakfast?src=hash&amp;ref_src=twsrc%5Etfw">#BBCBreakfast</a> how the ‘Homes for Ukraine’ scheme will work. <a href="https://t.co/CO0vkPsVh5">https://t.co/CO0vkPsVh5</a> <a href="https://t.co/vXcTGiifo1">pic.twitter.com/vXcTGiifo1</a></p>&mdash; BBC Breakfast (@BBCBreakfast) <a href="https://twitter.com/BBCBreakfast/status/1503286559755456513?ref_src=twsrc%5Etfw">March 14, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+			<blockquote class="twitter-tweet" data-partner="tweetdeck"><p lang="en" dir="ltr">More than two thousand companies, churches and individuals have offered to help host Ukrainian refugees fleeing the war. <br><br>Krish Kandiah from the Sanctuary Foundation tells <a href="https://twitter.com/hashtag/BBCBreakfast?src=hash&amp;ref_src=twsrc%5Etfw">#BBCBreakfast</a> the response has been unprecedented .<a href="https://t.co/JdS0rfuOox">https://t.co/JdS0rfuOox</a> <a href="https://t.co/trIAQjE5rF">pic.twitter.com/trIAQjE5rF</a></p>&mdash; BBC Breakfast (@BBCBreakfast) <a href="https://twitter.com/BBCBreakfast/status/1502174733630156800?ref_src=twsrc%5Etfw">March 11, 2022</a></blockquote>
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
 	</div>
 </div>
