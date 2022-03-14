7bd9eb34fc10b5a1e08ac397739eaaf9295aa7ce
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-14 20:26:17 +0000

    Update footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 	<footer class="footer">
 		<div class="footer_restrict flow">
-			<p>&copy; <?= date('Y') ?> Krish Kandiah &bull; <a href="/privacy">Privacy</a> &bull; Built by <a href="https://jackbarber.co.uk">Jack</a></p>
+			<p>&copy; <?= date('Y') ?> Krish Kandiah &bull; <a href="/privacy">Privacy</a> &bull; Built by <a href="https://jackbarber.co.uk">Jack</a> &amp; <a href="https://www.seoandy.com/">Andy</a></p>
 		</div>
 	</footer>
 
