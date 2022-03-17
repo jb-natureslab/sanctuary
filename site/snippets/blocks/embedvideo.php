be19fc6fa395024daf0d9d875716cde790c40fa0
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-17 10:10:53 +0000

    video

---
+++ site/snippets/blocks/embedvideo.php
@@ -0,0 +1,19 @@
+<?php
+$id = '4n-Wmru5LIA';
+?>
+<div class="wrapper wrapper--video">
+	<div class="wrapper--video_restrict flow">
+		<img src="/assets/images/video.svg" alt="Watch the Video" />
+		<iframe
+		  width="560"
+		  height="315"
+		  src="https://www.youtube.com/embed/<?= $id ?>&autoplay=1"
+		  srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/<?= $id ?>?autoplay=1><img src=https://img.youtube.com/vi/<?= $id ?>/hqdefault.jpg alt='Sanctuary Foundation'><span>▶</span></a>"
+		  frameborder="0"
+		  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+		  allowfullscreen
+		  title="Sanctuary Foundation" 
+		  loading="lazy"
+		></iframe>
+	</div>
+</div>
