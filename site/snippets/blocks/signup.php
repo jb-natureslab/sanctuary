c12123de7ebc901bf8b16182a3030b8476505b0c
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-18 22:46:45 +0000

    Update signup.php

--- site/snippets/blocks/signup.php
+++ site/snippets/blocks/signup.php
@@ -71,7 +71,7 @@
     </div>
     <div></div>
     <div id="sib-container" class="sib-container--large sib-container--vertical" style="text-align:center; background-color:rgba(255,255,255,1); max-width:540px; border-radius:3px; border-width:1px; border-color:#C0CCD9; border-style:solid;">
-      <form id="sib-form" method="POST" action="https://f473aaff.sibforms.com/serve/MUIEAA__TpmMZlmrsUhUO0t4qa3Yv8zrs41ST99zM0orqpTqwIQULM1jU6W7JKGEo-6bdciqZRBcLXzRoajzwMJxy5MmJ7DwUtwDzbOr2vVIPBWp-Fm9ZR38tGYONiil6Dz803cx7tC9dlchepI8C-KG4V-ZkDo8KqxMKzPKugc46lQTOripvo5wk-rU_Vs27KItZamZqGw0lOTH" data-type="subscription">
+      <form id="sib-form" method="POST" action="https://f473aaff.sibforms.com/serve/MUIEAOE2O42e5u-YFKM2sy_bMZeeQgfX0FqCO8whH9JPwRAqOPtSog9UuNFB6P9Kkju73jiVOnSnok-AzbJIsCXTF5r4AdDd7D5pH9UK2bFi39269d12xaxp9ce47kK1GZ89qY9vzQp4JphF0GP4dL204bqwqhmnlYm7Ou3wog12pm0VEEwv8FvNyIAyh5YyOECBvU3e4g81twhO" data-type="subscription">
         <div style="padding: 8px 0;">
           <div class="sib-form-block" style="font-size:32px; text-align:left; font-weight:700; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;">
             <p>Pledge Your Support</p>
@@ -158,13 +158,13 @@
                   </div>
                   <div class="entry__choice">
                     <label class="checkbox__label">
-                      <input type="checkbox" class="input_replaced" name="lists_31[]" data-value="Business such as a Pub, Restaurant or Shop" value="7" data-required="true" />
-                      <span class="checkbox checkbox_tick_positive"></span><span style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;">Business such as a Pub, Restaurant or Shop</span> </label>
+                      <input type="checkbox" class="input_replaced" name="lists_31[]" data-value="Church" value="4" data-required="true" />
+                      <span class="checkbox checkbox_tick_positive"></span><span style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;">Church</span> </label>
                   </div>
                   <div class="entry__choice">
                     <label class="checkbox__label">
-                      <input type="checkbox" class="input_replaced" name="lists_31[]" data-value="Church" value="4" data-required="true" />
-                      <span class="checkbox checkbox_tick_positive"></span><span style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;">Church</span> </label>
+                      <input type="checkbox" class="input_replaced" name="lists_31[]" data-value="Business such as a Pub, Restaurant or Shop" value="7" data-required="true" />
+                      <span class="checkbox checkbox_tick_positive"></span><span style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;">Business such as a Pub, Restaurant or Shop</span> </label>
                   </div>
                   <div class="entry__choice">
                     <label class="checkbox__label">
@@ -189,6 +189,47 @@
                 </div>
               </div>
               <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;">
+              </label>
+            </div>
+          </div>
+        </div>
+        <div style="padding: 8px 0;">
+          <div class="sib-form__declaration">
+            <div class="declaration-block-icon">
+              <svg class="icon__SVG" width="0" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                  <symbol id="svgIcon-sphere" viewBox="0 0 63 63">
+                    <path class="path1" d="M31.54 0l1.05 3.06 3.385-.01-2.735 1.897 1.05 3.042-2.748-1.886-2.738 1.886 1.044-3.05-2.745-1.897h3.393zm13.97 3.019L46.555 6.4l3.384.01-2.743 2.101 1.048 3.387-2.752-2.1-2.752 2.1 1.054-3.382-2.745-2.105h3.385zm9.998 10.056l1.039 3.382h3.38l-2.751 2.1 1.05 3.382-2.744-2.091-2.743 2.091 1.054-3.381-2.754-2.1h3.385zM58.58 27.1l1.04 3.372h3.379l-2.752 2.096 1.05 3.387-2.744-2.091-2.75 2.092 1.054-3.387-2.747-2.097h3.376zm-3.076 14.02l1.044 3.364h3.385l-2.743 2.09 1.05 3.392-2.744-2.097-2.743 2.097 1.052-3.377-2.752-2.117 3.385-.01zm-9.985 9.91l1.045 3.364h3.393l-2.752 2.09 1.05 3.393-2.745-2.097-2.743 2.097 1.05-3.383-2.751-2.1 3.384-.01zM31.45 55.01l1.044 3.043 3.393-.008-2.752 1.9L34.19 63l-2.744-1.895-2.748 1.891 1.054-3.05-2.743-1.9h3.384zm-13.934-3.98l1.036 3.364h3.402l-2.752 2.09 1.053 3.393-2.747-2.097-2.752 2.097 1.053-3.382-2.743-2.1 3.384-.01zm-9.981-9.91l1.045 3.364h3.398l-2.748 2.09 1.05 3.392-2.753-2.1-2.752 2.096 1.053-3.382-2.743-2.102 3.384-.009zM4.466 27.1l1.038 3.372H8.88l-2.752 2.097 1.053 3.387-2.743-2.09-2.748 2.09 1.053-3.387L0 30.472h3.385zm3.069-14.025l1.045 3.382h3.395L9.23 18.56l1.05 3.381-2.752-2.09-2.752 2.09 1.053-3.381-2.744-2.1h3.384zm9.99-10.056L18.57 6.4l3.393.01-2.743 2.1 1.05 3.373-2.754-2.092-2.751 2.092 1.053-3.382-2.744-2.1h3.384zm24.938 19.394l-10-4.22a2.48 2.48 0 00-1.921 0l-10 4.22A2.529 2.529 0 0019 24.75c0 10.47 5.964 17.705 11.537 20.057a2.48 2.48 0 001.921 0C36.921 42.924 44 36.421 44 24.75a2.532 2.532 0 00-1.537-2.336zm-2.46 6.023l-9.583 9.705a.83.83 0 01-1.177 0l-5.416-5.485a.855.855 0 010-1.192l1.177-1.192a.83.83 0 011.177 0l3.65 3.697 7.819-7.916a.83.83 0 011.177 0l1.177 1.191a.843.843 0 010 1.192z" fill="#0092FF"></path>
+                  </symbol>
+                </defs>
+              </svg>
+              <svg class="svgIcon-sphere" style="width:63px; height:63px;">
+                <use xlink:href="#svgIcon-sphere"></use>
+              </svg>
+            </div>
+            <p style="font-size:14px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#687484; background-color:transparent;">
+              We use Sendinblue as our marketing platform. By Clicking below to submit this form, you acknowledge that the information you provided will be transferred to Sendinblue for processing in accordance with their <a target="_blank" class="clickable_link" href="https://www.sendinblue.com/legal/termsofuse/">terms of use</a>
+            </p>
+          </div>
+
+        </div>
+        <div style="padding: 8px 0;">
+          <div class="sib-optin sib-form-block" data-required="true">
+            <div class="form__entry entry_mcq">
+              <div class="form__label-row ">
+                <label class="entry__label" style="font-size:16px; text-align:left; font-weight:700; font-family:&quot;Helvetica&quot;, sans-serif; color:#3c4858;" for="OPT_IN" data-required="*">
+                  Opt-in
+                </label>
+                <div class="entry__choice">
+                  <label>
+                    <input type="checkbox" class="input_replaced" value="1" id="OPT_IN" name="OPT_IN" required />
+                    <span class="checkbox checkbox_tick_positive"></span><span style="font-size:14px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;"><p>I agree to receive your newsletters and accept the data privacy statement.</p></span> </label>
+                </div>
+              </div>
+              <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;">
+              </label>
+              <label class="entry__specification" style="font-size:12px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#8390A4;">
+                You may unsubscribe at any time using the link in our newsletter.
               </label>
             </div>
           </div>
