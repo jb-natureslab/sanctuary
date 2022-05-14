2d35545631af135bb1cd74eb89f4c42577e24883
Jack Barber <jack@natureslaboratory.co.uk>
2022-05-14 19:33:13 +0100

    Update footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,10 +1,10 @@
 	<footer class="footer">
 		<div class="footer_restrict flow">
-			<p>Sanctuary Foundation is a charity and it has been entered onto the Register of Charities with the Registered Charity Number <a href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5197064" target="_blank">1198534</a>.</p>
-			<p>The project is being incubated by the charity <a href="https://www.stewardship.org.uk/" target="_blank">Stewardship</a>, who have opened a fund to receive donations to support this urgent response to the Ukraine crisis.</p>
-			<!--<p>Stewardship is the operating name of Stewardship Services (UKET) Limited. Established in 1906. Registered charity no. 234714, registered in England and Wales, and a company limited by guarantee, no. 90305, registered in England and Wales. Registered office: 1 Lamb's Passage, London EC1Y 8AB. Stewardship is <a href="https://www.fundraisingregulator.org.uk/directory/stewardship-services-uket-limited">registered with the Fundraising Regulator</a>.</p>-->
+			<p>Sanctuary Foundation is a charity and it has been entered onto the Register of Charities with the Registered Charity Number 1198534</p>
+			<p>The project was incubated and facilitated by the charity Stewardship</p>
+			<p>Registered with Fundraising Regulator</p>
 			<img src="/assets/images/frsb-logo.jpg" alt="Funraising Regulator Logo" />
-			<p>&copy; <?= date('Y') ?> <a href="https://krishk.com" target="_blank">Krish Kandiah</a> &bull; <a href="/privacy">Privacy</a> &bull; Built by <a href="https://jackbarber.co.uk">Jack</a> &amp; <a href="https://www.seoandy.com/">Andy</a></p>
+			<p>&copy; <?= date('Y') ?> Sanctuary Foundation &bull; <a href="/privacy">Privacy</a> &bull; Built by <a href="https://jackbarber.co.uk">Jack</a></p>
 		</div>
 	</footer>
 
